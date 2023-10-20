5ac7a88ffe858bea5d2b8e2c8897d29c70e65be0
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-10-20 22:35:13 +0100

    chore: remove unused exception chore: create common base class for all exceptions

--- src/simple_logging_config/_exceptions.py
+++ src/simple_logging_config/_exceptions.py
@@ -2,23 +2,20 @@
 Exceptions raised by SimpleLoggingConfig
 """
 
+
 class SLCException(Exception):
-    pass
+    """Base class for all simple_logging_config exceptions"""
 
 
 class LoggingConfigException(SLCException):
     """Exception raised for an invalid config resource name"""
+
     def __init__(self, config_name):
         super().__init__(f"Invalid Logging Config: {config_name}")
 
 
-class LoggingResourceException(SLCException):
-    """Exception raised for an invalid config resource name"""
-    def __init__(self, resource_name):
-        super().__init__(f"Invalid Resource Name: {resource_name}")
-
-
 class LoggingHandlerException(SLCException):
-    """Exception raised for an invalid config resource name"""
+    """Exception raised for an invalid handler name"""
+
     def __init__(self, handler_name):
         super().__init__(f"Invalid Handler: {handler_name}")

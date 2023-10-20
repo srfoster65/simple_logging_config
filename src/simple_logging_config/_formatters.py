73baf1b09eefcef686d618b1d767ae8409b60e22
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-10-20 22:34:18 +0100

    chore: update logging in formatters.py

--- src/simple_logging_config/_formatters.py
+++ src/simple_logging_config/_formatters.py
@@ -3,10 +3,13 @@ Functions related to modifying logging formatters
 
 Overide default formatters for specific handlers with new format read from environment variables.
 """
-
+import logging
 from os import environ
 
 from ._defaults import CUSTOM_FORMAT_TEMPLATE_ENV
+
+
+logger = logging.getLogger(__name__)
 
 
 def modify_formatters(config_data: dict) -> None:
@@ -14,10 +17,12 @@ def modify_formatters(config_data: dict) -> None:
     Overide default formatters for specific handlers with new format read from environment variables.
     """
     for handler_name in config_data["handlers"]:
-        format_str = environ.get(
-            CUSTOM_FORMAT_TEMPLATE_ENV.format(handler_name=handler_name)
-        )
+        logger.debug(CUSTOM_FORMAT_TEMPLATE_ENV)
+        env_name =  CUSTOM_FORMAT_TEMPLATE_ENV.format(handler_name=handler_name).upper()
+        logger.debug("Checking formatter: %s = %s", handler_name, env_name)
+        format_str = environ.get(env_name)
         if format_str is not None:
+            logger.debug("Updating formatter: %s = %s", handler_name, format_str)
             formatter_name = "custom_" + handler_name
             # Add a custom formatter
             config_data["formatters"][formatter_name] = {}

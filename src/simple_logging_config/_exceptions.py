0ab2e34c582fe8fda9f0e223762c08fd3e117e4f
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-10-20 08:47:06 +0100

    chore: add InvalidHandlerException

--- src/simple_logging_config/_exceptions.py
+++ src/simple_logging_config/_exceptions.py
@@ -13,3 +13,9 @@ class InvalidResourceException(Exception):
     """Exception raised for an invalid config resource name"""
     def __init__(self, resource_name):
         super().__init__(f"Invalid Resource Name: {resource_name}")
+
+
+class InvalidHandlerException(Exception):
+    """Exception raised for an invalid config resource name"""
+    def __init__(self, handler_name):
+        super().__init__(f"Invalid Handler: {handler_name}")

87d85408a48c4960d79bf900c3470ad79a11f60b
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-10-22 18:44:56 +0100

    add test for specifying logs folder

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,12 +1,26 @@
+
+
 """
 Test support for logging to file functions.
 
 99% tested by default configuration
 """
 
+from os import makedirs
 from pathlib import Path
 
+import pytest
+
 from simple_logging_config import configure_logging
+
+
+@pytest.fixture(scope="function", autouse=True)
+def configure():
+    """
+    Ensure logging is reset after each test.
+    """
+    yield
+    configure_logging().reset()
 
 
 class TestLogFileSettings:
@@ -21,3 +35,12 @@ class TestLogFileSettings:
         log_file_path_as_filename = "test_as_filename.log"
         configure_logging(log_file_path=log_file_path_as_filename)
         assert Path(log_file_path_as_filename).is_file()
+
+    def test_log_file_path_as_path(self):
+        """
+        Test log file path can be set as a folder
+        """
+        log_file_path_as_path = "logs_folder"
+        makedirs(Path(log_file_path_as_path), exist_ok=True)
+        configure_logging(log_file_path=log_file_path_as_path)
+        assert Path(log_file_path_as_path).is_dir()

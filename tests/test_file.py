99f09902db036916d1cd138ec96c83274eec1aa1
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-10-22 20:17:24 +0100

    add coverage for backup_count

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,17 +1,16 @@
-
-
 """
 Test support for logging to file functions.
 
 99% tested by default configuration
 """
 
-from os import makedirs
+from os import makedirs, listdir
 from pathlib import Path
+import logging
 
 import pytest
 
-from simple_logging_config import configure_logging
+from simple_logging_config import configure_logging, rotate_log
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -40,7 +39,45 @@ class TestLogFileSettings:
         """
         Test log file path can be set as a folder
         """
-        log_file_path_as_path = "logs_folder"
+        log_file_path_as_path = "log_file_path_as_folder"
         makedirs(Path(log_file_path_as_path), exist_ok=True)
         configure_logging(log_file_path=log_file_path_as_path)
         assert Path(log_file_path_as_path).is_dir()
+
+    def test_rotating_limits_to_5_backups(self):
+        """
+        Test rotating handler creates 5 backups
+        """
+        log_file_path_as_path = "log_file_path_rotating_5"
+        backup_count = 5  # default
+        makedirs(Path(log_file_path_as_path), exist_ok=True)
+        configure_logging(config="rotating_file", log_file_path=log_file_path_as_path)
+        logger = logging.getLogger(__name__)
+        loop = backup_count + 2
+        expected = backup_count + 1  # test.log + test.log.1 .. test.log.5
+        for i in range(0, loop):
+            logger.debug("loop %s", i)
+            rotate_log()
+        backup_file_count = len(listdir(log_file_path_as_path))
+        assert backup_file_count == expected
+
+    def test_setting_numbr_of_backup_files(self):
+        """
+        Test setting number of backup files
+        """
+        log_file_path_as_path = "log_file_path_rotating_3"
+        backup_count = 3
+        makedirs(Path(log_file_path_as_path), exist_ok=True)
+        configure_logging(
+            config="rotating_file",
+            backup_count=backup_count,
+            log_file_path=log_file_path_as_path,
+        )
+        logger = logging.getLogger(__name__)
+        loop = backup_count + 2
+        expected = backup_count + 1  # test.log + test.log.1 .. test.log.3
+        for i in range(0, loop):
+            logger.debug("loop %s", i)
+            rotate_log()
+        backup_file_count = len(listdir(log_file_path_as_path))
+        assert backup_file_count == expected

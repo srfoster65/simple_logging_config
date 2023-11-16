0dd3d745fcfa7a24e25235ac32d97224a6a24313
srfoster65 <135555068+srfoster65@users.noreply.github.com>
2023-11-16 09:12:26 +0000

    chore: annotate _configure.py

--- src/simple_logging_config/_configure.py
+++ src/simple_logging_config/_configure.py
@@ -2,9 +2,11 @@
 Helper functions
 """
 
+from typing import Any
+
 from ._simple_logging_config import SimpleLoggingConfig
 
 
-def configure_logging(**kwargs) -> SimpleLoggingConfig:
+def configure_logging(**kwargs: Any) -> SimpleLoggingConfig:
     """External API to initialise logging."""
     return SimpleLoggingConfig(**kwargs)
